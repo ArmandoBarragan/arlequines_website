5c2414969fb64d38732fc199c9642bc93180c346
Armando Barragan <armando.barragan@benandfrank.com>
2025-10-23 09:28:55 -0600

    Added foreign key relationship
    The previous state of the Presentation model created no foreign key, which allowed
    the user to create presentations with non-existent plays. This commit adds
    the constraint

--- app/src/models/presentation.go
+++ app/src/models/presentation.go
@@ -6,7 +6,8 @@ import (
 
 type Presentation struct {
 	ID             uint      `gorm:"primaryKey" json:"id"`
-	PlayID         uint      `json:"play_id"`
+	PlayID         uint      `json:"play_id" gorm:"not null;index"`
+	Play           Play      `gorm:"foreignKey:PlayID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT" json:"play,omitempty"`
 	DateTime       time.Time `json:"datetime"`
 	Location       string    `json:"location"`
 	Price          float64   `json:"price"`
